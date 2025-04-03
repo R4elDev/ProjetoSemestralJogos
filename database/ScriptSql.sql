5570e14eda2ae4b13dd0c4ac4a0e483b5649c3b7
ISRAEL MAGALHÃES DOS SANTOS JUNIOR <24122293@CFP127ALUNO.LOCAL>
2025-04-03 17:31:44 -0300

    Adicionando tabelas

--- database/ScriptSql.sql
+++ database/ScriptSql.sql
@@ -17,6 +17,36 @@ create table tbl_jogo(
 	link 				varchar(200)
 );
 
+create table  tbl_plataforma(
+	id                        int not null primary key auto_increment,
+    tipo_de_plataforma        varchar(100) not null,
+    logo                      varchar(200) not null
+);
+
+create table tbl_versao(
+	id                        int not null primary key auto_increment,
+	tipo_de_versao            varchar(45) not null
+);
+
+create table tbl_genero(
+	id                        int not null primary key auto_increment,
+    tipo_de_genero            varchar(100) not null
+);
+
+create table tbl_classificacao(
+	id                        int not null primary key auto_increment,
+    tipo_de_classificacao     varchar(5) not null
+);
+
+create table tbl_desenvolvedora(
+	id                        int not null primary key auto_increment,
+    logo                      varchar(200) not null,
+    link                      varchar(200) not null
+);
+
+
+
+
 show tables;
 desc tbl_jogo;
 select * from tbl_jogo;
