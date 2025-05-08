3e6ad3697134103381bfae42679d90a5b1c4a89e
R4elDev <raraeldev@gmail.com>
2025-05-07 22:37:20 -0300

    Realizando o DAO da tabela PlataformaJogo

---
+++ model/DAO/plataforma_jogo.js
@@ -0,0 +1,120 @@
+/*******************************************************************************************************
+ * Objetivo: Criar a comunicação com o Banco de Dados para fazer o CRUD de Plataforma_Jogo
+ * Data: 07/05/2025
+ * Autor: Israel
+ * Versão: 1.0
+ ******************************************************************************************************/
+
+//import da biblioteca do prisma client para executar os scripts SQL
+const { PrismaClient } = require('@prisma/client')
+
+//Instancia (criar um objeto a ser utilizado) a biblioteca do prisma/client
+const prisma = new PrismaClient()
+
+
+//Função para inserir um nova PlataformaJogo
+const insertPlataformaJogo = async function(PlataformaJogo){
+    try {
+  
+        let sql = `insert into tbl_plataforma_jogo  (id_plataforma,id_jogo,hardware) 
+        values(
+            '${PlataformaJogo.id_filme}',
+            '${PlataformaJogo.id_genero}',
+            '${PlataformaJogo.hardware}'
+        );`
+  
+        //Executa o scriptSQL no banco de dados e aguarda o retorno do BD para 
+        //saber se deu certo                                  
+        let result = await prisma.$executeRawUnsafe(sql)
+  
+        if(result){
+            return true
+        }else{
+            return false
+        }
+    } catch (error) {
+        return false
+    }
+}
+
+//Função para atualizar uma PlataformaJogo existente
+const updatePlataformaJogo = async function(PlataformaJogo){
+    try {
+        let sql = `update tbl_plataforma_jogo set     id_plataforma       = '${PlataformaJogo.id_plataforma}',
+                                                      id_jogo             = '${PlataformaJogo.id_jogo}',
+                                                      hardware            = '${PlataformaJogo.hardware}'
+                                                      where id = ${PlataformaJogo.id}`
+        let resultPlataformaJogo = await prisma.$executeRawUnsafe(sql)
+  
+        if(resultPlataformaJogo){
+            return true
+        }else{
+            return false
+        }
+    } catch (error) {
+      return false
+    }
+}
+
+//Função para excluir um FilmeGenero existente
+const deletePlataformaJogo = async function(id){
+    try {
+      let sql = `delete from tbl_plataforma_jogo where id = ${id}`
+  
+      let result = await prisma.$executeRawUnsafe(sql)
+  
+      if (result){
+        return true
+      }else{
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+}
+
+//Função para retornar todos os FilmeGeneros existentes
+const selectAllPlataformaJogo = async function(){
+
+    try {
+      //ScriptSQL para retornar todos os dados
+      let sql = 'select * from tbl_plataforma_jogo order by id desc'
+
+      //Executa o scriptSQL no BD e aguarda o retorno dos dados
+      let result = await prisma.$queryRawUnsafe(sql)
+
+      if(result){
+        return result
+      }else{
+        return false
+      }
+
+    } catch (error) {
+      return false
+    }
+}
+
+//Função para buscar um FilmeGenero pelo ID
+const selectByIdPlataformaJogo = async function(id){
+    try {
+      let sql = `select * from tbl_plataforma_jogo where id = ${id}`
+  
+      let result = await prisma.$queryRawUnsafe(sql)
+  
+      if(result){
+        return result
+      }else{
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+}
+
+module.exports = {
+    insertPlataformaJogo,
+    updatePlataformaJogo,
+    deletePlataformaJogo,
+    selectAllPlataformaJogo,
+    selectByIdPlataformaJogo
+}
